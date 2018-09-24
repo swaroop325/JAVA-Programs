173bff91e131b3f2de6f18551cca91b9e7edb418
Swaroop A S <swaroopanand325@gmail.com>
2018-09-24 11:31:35 +0530

    Update 2.java

--- 2.java
+++ 2.java
@@ -11,6 +11,6 @@ class FactorialExample{
   for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
-  System.out.println("Factorial of "+number+" is: "+fact);    
+  System.out.println(fact);    
  }  
 }  
