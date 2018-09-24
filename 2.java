5cc5093a4c9d7b9840bb67ff0ef2cbe72294ce30
Swaroop A S <swaroopanand325@gmail.com>
2018-09-24 11:29:48 +0530

    Update 2.java

--- 2.java
+++ 2.java
@@ -2,26 +2,15 @@ import java.lang.*;
 import java.io.*; 
 import java.util.*; 
   
-// Class of ReverseString 
-class ReverseString 
-{ 
-    public static void main(String[] args) 
-    { 
-        String input = "GeeksforGeeks"; 
-  
-        // getBytes() method to convert string  
-        // into bytes[]. 
-        byte [] strAsByteArray = input.getBytes(); 
-  
-        byte [] result =  
-                   new byte [strAsByteArray.length]; 
-  
-        // Store result in reverse order into the 
-        // result byte[] 
-        for (int i = 0; i<strAsByteArray.length; i++) 
-            result[i] =  
-             strAsByteArray[strAsByteArray.length-i-1]; 
-  
-        System.out.println(new String(result)); 
-    } 
-} 
+class FactorialExample{  
+ public static void main(String args[]){  
+  int i,fact=1;  
+  int number;
+   Scanner sc=new Scanner(System.in);
+   number=sc.nextInt();//It is the number to calculate factorial    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println("Factorial of "+number+" is: "+fact);    
+ }  
+}  
